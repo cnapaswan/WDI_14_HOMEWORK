b382a8b1e5aba5228d032b87182f70a2fe35374e
mia <c.napaswan@gmail.com>
2017-12-02 17:29:09 +1100

    2nd commit

--- Mia/wk01/4-thu/pt.js
+++ Mia/wk01/4-thu/pt.js
@@ -1,82 +1,136 @@
 var origin = prompt("Please insert origin")
 var destination = prompt("please insert destination")
 
-/** do the clean up later (apply to variables too)
-var cleanUserInput = function(){
-myString.replace(/[^A-Z0-9]/ig, "").toLowerCase();
-removeChar.split('').join('');
-}
+var stations = [
+["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"], 
+["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong", "Tooronga"], 
+["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"]
+]
 
-**/
-
-/**
-//21 routes in total
-12 = frontback ala
-34 = frontback glen
-56 = frontback sand
-(11)(12)    (13)(14)     (15)(16)
-(22)        (23)  (24)(25)(26)
-(33)(34)    (35)(36)
-(44)        (45)  (46)
-(55)(56)
-(66)
-
-6+5+4+3+2+1
-**/
-
-
-//route within the line variables
-
-var frontAla = ["Flinders Street", "Richmond"] //11
-var inAla = ["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"]//12
-var backAla = ["East Richmond", "Burnley", "Hawthorn", "Glenferrie"]//22
-
-var frontGlen = ["Flagstaff", "Melbourne Central", "Parliament", "Richmond"]//33
-var inGlen = ["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong", "Tooronga"]//34
-var backGlen = ["Kooyong", "Tooronga"]//44
-
-var frontSand = ["Southern Cross", "Richmond"]//55
-var inSand = ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"]//56
-var backSand = ["South Yarra", "Prahran", "Windsor"]//66
-
-//route changing at richmond
-//var froAlafroGlen = frontAla.concat(frontGlen) //13
-//var froAlaBaGlen = frontAla.concat(backGlen) //14
-// do I really have to do all 21???
-
-
-var stations =[frontAla, backAla, frontGlen, backGlen, frontSand, backSand]
-
-var findStations = function(ori,des){
+var findStations = function(){
+  // ORIGIN
   var originFound = false
-  var destinationFound = false
-  
+  var originLine
+  var originArray
+
   for(i = 0; i < stations.length; i++){
     if (stations[i].includes(origin)) {
-      originFound = true
-
+        originFound = true
+        originLine = i
+        originArray = stations[i]
     } 
-    if (stations[i].includes(destination)) {
-       destinationFound = true
-    }
   }
 
   if(originFound === false){
     document.getElementById("start").innerHTML = "origin: " + " " + "please insert correct station"; 
+  }
+  // DESTINATION
+  var destinationFound = false
+  var destinationLine
+  var destinationArray
+  
+  for(i = 0; i < stations.length; i++){
+    if (stations[i].includes(destination)) {
+      destinationFound = true
+      destinationLine = i
+      destinationArray = stations[i]
     }
-  if(destinationFound === false){
+  }
+
+  if(destinationFound === false) {
     document.getElementById("finish").innerHTML = "destination: " + " " + "please insert correct station"; 
   }
+
+  if(originFound === false || destinationFound === false){
+    return 
+  }
+  var completeArray
+
+  if (origin === destination) {
+    document.getElementById("routy").innerHTML = "You are not going anywhere"
+    return 
+  }
+
+  if (origin === 'Richmond'){
+    var oriPosiInnerArr = destinationArray.indexOf(origin)
+    var desPotiInnerArr = destinationArray.indexOf(destination)
+
+    if (oriPosiInnerArr > desPotiInnerArr){
+      completeArray = destinationArray.slice(desPotiInnerArr, (oriPosiInnerArr + 1)).reverse()
+
+    } else if (oriPosiInnerArr < desPotiInnerArr){
+      completeArray = destinationArray.slice(oriPosiInnerArr,(desPotiInnerArr + 1))
+
+    }
+  } else if (destination === 'Richmond'){
+    var oriPosiInnerArr = originArray.indexOf(origin)
+    var desPotiInnerArr = originArray.indexOf(destination)
+
+    if (oriPosiInnerArr > desPotiInnerArr){
+      completeArray = originArray.slice(desPotiInnerArr, (oriPosiInnerArr + 1)).reverse()
+
+    } else if (oriPosiInnerArr < desPotiInnerArr){
+      completeArray = originArray.slice(oriPosiInnerArr,(desPotiInnerArr + 1))
+
+    }
+  }
+
+  else if (originLine === destinationLine){
+    var oriPosiInnerArr = originArray.indexOf(origin)
+    var desPotiInnerArr = destinationArray.indexOf(destination)
+
+    if (oriPosiInnerArr > desPotiInnerArr){
+      completeArray = originArray.slice(desPotiInnerArr, (oriPosiInnerArr + 1)).reverse()
+
+    } else if (oriPosiInnerArr < desPotiInnerArr){
+      completeArray = originArray.slice(oriPosiInnerArr,(desPotiInnerArr + 1))
+
+    }
+  }
+
+
+    //diferent line
+    else if (originLine !== destinationLine) { 
+
+      var oriInOriArray = originArray.indexOf(origin)
+      var richInOriArray = originArray.indexOf('Richmond')
+
+      var richInDesArray = destinationArray.indexOf('Richmond')
+      var desInDesArray = destinationArray.indexOf(destination)
+
+      var oriCompleteArr
+      var desCompleteArr
+
+      if (oriInOriArray < richInOriArray){
+        oriCompleteArr = originArray.slice(oriInOriArray, richInOriArray)
+      } else if (richInOriArray < oriInOriArray){
+        oriCompleteArr = originArray.slice(richInOriArray, (oriInOriArray +1)).reverse()
+      }
+
+      if (richInDesArray < desInDesArray) {
+        desCompleteArr = destinationArray.slice(richInDesArray, (desInDesArray +1))
+      } else if (desInDesArray < richInDesArray){
+        desCompleteArr = destinationArray.slice(desInDesArray,richInDesArray).reverse()
+      }
+
+      completeArray = oriCompleteArr.concat(desCompleteArr)
+
+    }
+    
+    document.getElementById("journey").innerHTML = completeArray.join(' -----> ')
+
+
+    if ((completeArray.length -1) > 1){
+      document.getElementById("stop").innerHTML = (completeArray.length -1) + " stops in total"
+    } else {
+      document.getElementById("stop").innerHTML = (completeArray.length -1) + " stop in total"
+    }
+
+
+
+    document.getElementById("start").innerHTML = "origin: " + origin; 
+    document.getElementById("finish").innerHTML = "destination: " + destination; 
+
 }
 
-
-
-document.getElementById("start").innerHTML = "origin: " + origin; 
-document.getElementById("finish").innerHTML = "destination: " + destination; 
-
 findStations()
-
-
-//NEXT TO DO
-//Print the journey .....
-//document.getElementById("routy").innerHTML = backAla.join(" -----> ")
