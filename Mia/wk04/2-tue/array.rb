9ce99374ab7feb508676e58afb5a6d546a824b3f
mia <c.napaswan@gmail.com>
2017-12-19 21:51:03 +1100

    completed

--- Mia/wk04/2-tue/array.rb
+++ Mia/wk04/2-tue/array.rb
@@ -1,74 +1,82 @@
 #Arrays
-
-
 planeteers = ["Earth", "Wind", "Captain Planet", "Fire", "Water"]
+
 #Access the second value in planeteers.
 planeteers[1]
+
 #Add "Heart" to the end of planeteers.
 planeteers.push("Heart")
+
 #Remove "Captain Planet" from the array (without using a method).
-planeteers.delete("Captain Planet")
+byecap = ["Captain Planet"]
+planeteers - byecap
 
 #Combine planeteers with 
 rangers = ["Red", "Blue", "Pink", "Yellow", "Black"] 
+
 #and save the result in a variable called heroes.
-heroes = planeteers + rangers #["Earth", "Wind", "Fire", "Water", "Heart", "Red", "Blue", "Pink", "Yellow", "Black"]
+heroes = planeteers + rangers 
+#["Earth", "Wind", "Fire", "Water", "Heart", "Red", "Blue", "Pink", "Yellow", "Black"]
 
 #Alphabetize the contents of heroes using a method. The Ruby documentation might help.
-
-def alphabetize(arr)
-  return arr.sort
-end
-
-alphabetize(heroes)
+heroes.sort
 
 #Randomize the contents of heroes using a method. The Ruby documentation might help.
-def everydayimshuffling(arr)
-  return arr.shuffle
-end
-everydayimshuffling(heroes)
+heroes.shuffle
 
 #Bonus
 #Select a random element from heroes using a method. The Ruby documentation might help.
-def pikmebabeonemoretime(arr)
-  return arr.sample
-end
-pikmebabeonemoretime(heroes)
+heroes.sample
 
-Select all elements in heroes that begin with "B" using a method. The Ruby documentation might help.
-
+#Select all elements in heroes that begin with "B" using a method. The Ruby documentation might help.
+def bwhere(arr)
 those_b = []
-def bwhere (arr)
   arr.each do |elem|
     if(elem.start_with?('B'))
       those_b.push(elem)
-
+    end
   end
-  return
+  those_b
 end
-# Your answer here
-Hashes
 
-Initialize a hash called ninja_turtle with the properties name, weapon and radical. They should have values of "Michelangelo", "Nunchuks" and true respectively.
+bwhere(heroes)
 
-# Your answer here
-Add a key age to ninja_turtle. Set it to whatever numerical value you'd like.
 
-# Your answer here
-Remove the radical key-value pair from ninja_turtle.
+#Hashes
+#Initialize a hash called ninja_turtle with the properties name, weapon and radical. 
+#They should have values of "Michelangelo", "Nunchuks" and true respectively.
+ninja_turtle = {
+  name: "Michelangelo",
+  weapon: "Nunchuks",
+  radical: true
+}
 
-# Your answer here
-Add a key pizza_toppings to ninja_turtle. Set it to an array of strings (e.g., ["cheese", "pepperoni", "peppers"]).
+#Add a key age to ninja_turtle. Set it to whatever numerical value you'd like.
+ninja_turtle[:age] = 3
 
-# Your answer here
-Access the first element of pizza_toppings.
+#Remove the radical key-value pair from ninja_turtle.
+ninja_turtle.delete(:radical)
 
-# Your answer here
-Produce an array containing all of ninja_turtle's keys using a method. The Ruby documentation might help.
+#Add a key pizza_toppings to ninja_turtle. 
+#Set it to an array of strings (e.g., ["cheese", "pepperoni", "peppers"]).
+ninja_turtle[:pizza_toppings] = ["pineapple", "bacon rasher", "pepperoni"]
 
-# Your answer here
-Bonus
+#Access the first element of pizza_toppings.
+ninja_turtle[:pizza_toppings][0]
 
-Print out each key-value pair in the following format - "KEY's is equal to VALUE" -- using a method. The Ruby documentation might help.
 
-# Your answer here
+#Produce an array containing all of ninja_turtle's keys using a method. 
+ninja_turtle.keys
+
+
+#Bonus
+#Print out each key-value pair in the following format
+# - "KEY's is equal to VALUE" -- using a method. 
+k = ninja_turtle.keys
+v = ninja_turtle.values
+
+for i in 0..3
+puts "#{k[i]} is equal to #{v[i]}"
+end
+
+
