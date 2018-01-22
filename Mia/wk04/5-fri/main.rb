6fa994cd0db6544785e4d5ed97ea16493220c514
mia <c.napaswan@gmail.com>
2018-01-23 00:06:23 +1100

    stock api hw

--- Mia/wk04/5-fri/main.rb
+++ Mia/wk04/5-fri/main.rb
@@ -1,6 +1,21 @@
-require = 'sinatra'
-require = 'sinatra/reloader'
+require 'sinatra'
+require 'stock_quote'
+require 'sinatra/reloader'
+
 
 get '/' do
-
+  stock = StockQuote::Stock.json_quote(params["stock"])
+  @name = stock["name"]
+  @symbol = stock["symbol"]
+  @exchange = stock["exchange"]
+  @l = stock["l"]
+  @op = stock["op"]
+  @hi = stock["hi"]
+  @lo = stock["lo"]
+  @vo = stock["vo"]
+  @c = stock["c"]
+  @cp = stock["cp"]
+  
+  erb :index
 end
+
