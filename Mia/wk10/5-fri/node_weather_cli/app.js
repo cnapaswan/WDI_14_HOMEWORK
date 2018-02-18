b093e6eccd9383992758e9cb598ef16b93ebf9b0
mia <c.napaswan@gmail.com>
2018-02-18 16:59:36 +1100

    node weather CLI

---
+++ Mia/wk10/5-fri/node_weather_cli/app.js
@@ -0,0 +1,13 @@
+var request = require('request');
+var apikey = "bc6cb2e7e1e88f304611203162b9a424"
+var city = process.argv[2]
+
+
+url= "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apikey + "&units=metric"
+
+request({
+    url: url,
+    json: true
+}, function (error, response, body) {
+    console.log(city + " current temperature is " + body.main.temp)
+})
